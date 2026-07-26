2684e6666f47c73d79d963f8bc43bf1c73efde59
azanashiq429-png <azanashiq429@gmail.com>
2026-07-26 10:48:34 +0500

    Create keys.js

---
+++ keys.js
@@ -0,0 +1,26 @@
+// keys.js - ScholarCraft Secure Key Registry Database
+const KEY_DATABASE_REGISTRY = {
+    // 1 Month Plan Keys (Valuation: 300 PKR)
+    tierOneMonth: [
+        'M1X7R2A', 
+        'K4P9W7Z', 
+        'Q9L5T1X', 
+        'B7V3C8N'
+    ],
+
+    // 3 Month Plan Keys (Valuation: 1000 PKR)
+    tierThreeMonth: [
+        'X8N2M9V', 
+        'L1C4K7B', 
+        'P3Q9W5Z', 
+        'R7T2Y6U'
+    ],
+
+    // 1 Year Plan Keys (Valuation: 3000 PKR)
+    tierOneYear: [
+        'Z9W4X2Y', 
+        'V7B3N8M', 
+        'K1L6C4P', 
+        'Q3R8T5V'
+    ]
+};
